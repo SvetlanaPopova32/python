d4dc352a623bbd0d5a596bd1133dd8cbaa0a3d90
SvetlanaPopova32 <84283846+SvetlanaPopova32@users.noreply.github.com>
2021-06-11 17:30:42 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,8 +1,18 @@
-my_f = open("text.txt", "w")
+class Matrix:
+    def __init__(self, list_of_lists):
+        self.list_of_lists = list_of_lists
 
-while True:
-    content = (input())
-    my_f.writelines(content + '\n')
-    if content == " ":
-        break
-my_f.close()
+    def __str__(self):
+        return '\n'.join(map(str, self.list_of_lists))
+
+    def __add__(self, other):
+        for x in range(len(self.list_of_lists)):
+            for y in range(len(self.list_of_lists[x])):
+                self.list_of_lists[x][y] = self.list_of_lists[x][y] + other.list_of_lists[x][y]
+        return Matrix.__str__(self)
+
+
+lst = Matrix([[-1, 6, 1], [-4, 0, 1], [7, 1, -1], [1, 5, -7]])
+
+lst_2 = Matrix([[1, 7, 9], [-7, 9, 2], [0, 2, -2], [2, 2, -7]])
+print(lst.__add__(lst_2))
--- 3.py
+++ 3.py
@@ -1,8 +1,34 @@
-with open ('text_3.txt', 'r', encoding='utf-8') as my_file:
-    my_list = []
-    for line in my_file:
-        a = line. split()
-        if float(a[1]) < 20000:
-            print(a[0])
-        my_list.append(float(a[1]))
-print(sum(my_list)/len(my_list))
+class Cell:
+
+    def __init__(self, c_number):
+        self.c_number = c_number
+
+    def __add__(self, other):
+        return self.c_number + other.c_number
+
+    def __sub__(self, other):
+        if self.c_number > other.c_number:
+            return self.c_number - other.c_number
+        else:
+            raise ValueError
+
+    def __mul__(self, other):
+        return self.c_number * other.c_number
+
+    def __truediv__(self, other):
+        return round(self.c_number / other.c_number)
+
+    def make_order(self, line):
+        for i in range(int(self.c_number) // line):
+            print('*' * line)
+        print('*' * (int(self.c_number) % line))
+
+
+cell_1 = Cell(29)
+cell_2 = Cell(2)
+print(cell_1 + cell_2)
+print(cell_1 - cell_2)
+print(cell_1 / cell_2)
+print(cell_1 * cell_2)
+print(cell_1.make_order(5))
+
